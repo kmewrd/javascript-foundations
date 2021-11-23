0c60fb17e218b127fb59154494840ece66570d92
Kim Ward <kmewrd@outlook.com>
2021-11-23 16:24:12 -0600

    Complete dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
-
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.food = 0;
+    this.hungry = true;
+  }
+  greet(rider) {
+    return `Hi, ${this.rider}!`;
+  }
+  // Unsure about code below this line
+  eat() {
+    this.food = this.food + 1;
+    if (this.food < 3) {
+      this.hungry = true;
+    } else {
+      this.hungry = false;
+    }
+  }
+}
+module.exports = Dragon;
