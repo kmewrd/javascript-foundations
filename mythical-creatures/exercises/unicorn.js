cf379eb2a503af3afaca1941a1d96601b57ecb55
Kim Ward <kmewrd@outlook.com>
2021-11-22 18:14:17 -0600

    Complete unicorn test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,28 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    if (color === undefined) {
+      this.color = "white";
+    } else {
+      this.color = color;
+    }
+    this.isWhite = false;
+  }
+  isWhite() {
+    if (this.color === "white") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  says(sparkle) {
+    return(`**;* ${sparkle} *;**`);
   }
 }
 
 module.exports = Unicorn;
+
+// 1. read the test
+// 2. ask yourself if it's going to fail and why
+// 3. run the test
+// 4. make it pass
