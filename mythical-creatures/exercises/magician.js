38dba886aaad1ad94f22b2b28183cc26a8a40f7d
Kim Ward <kmewrd@outlook.com>
2021-12-10 09:26:06 -0600

    Change === false to bang operator

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -11,14 +11,14 @@ class Magician {
   }
   performTrick() {
     this.confidencePercentage += 10;
-    if (this.favoriteAccessory == "top hat") {
+    if (this.favoriteAccessory === "top hat") {
       return "PULL RABBIT FROM TOP HAT";
     } else {
       return "PULL DOVE FROM SLEEVE";
     }
   }
   performShowStopper() {
-    if (this.confidencePercentage < 100 || this.assistant == false) {
+    if (this.confidencePercentage < 100 || !this.assistant) {
       return "Oh no, this trick is not ready!";
     } else {
       return "WOW! The magician totally just sawed that person in half!";
