46cff3c57b6d424d660da3aa6df4c2e6a584b212
Kim Ward <kmewrd@outlook.com>
2021-12-14 09:24:34 -0600

    Complete medusa test

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,22 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(victim) {
+    if (this.statues.length < 3) {
+      this.statues.push(new Statue(victim.name));
+    } else {
+      var person = this.statues.shift();
+      person = new Person(person.name);
+      person.mood = 'relieved';
+      this.statues.push(new Statue(victim.name));
+      return person;
+    }
+  }
+};
+
+module.exports = Medusa;
