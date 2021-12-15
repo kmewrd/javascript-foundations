49754390517860ee55ccc73e2628c35dab5b4ae1
Kim Ward <kmewrd@outlook.com>
2021-12-14 22:16:24 -0600

    Refactor vampire exercise

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,11 +1,7 @@
 class Vampire {
   constructor(name, pet) {
     this.name = name;
-    if (pet === undefined) {
-      this.pet = "bat";
-    } else {
-    this.pet = pet;
-    }
+    this.pet = pet || "bat";
     this.ouncesDrank = 0;
     this.thirsty = true;
   }
