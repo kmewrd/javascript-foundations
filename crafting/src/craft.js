696f9eb280fccc88385e1a776a3f417d30e20915
Kim Ward <kmewrd@outlook.com>
2021-12-20 14:11:19 -0600

    Complete craft test

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,20 @@
 class Craft {
-
+  constructor(craft) {
+    this.name = craft.type;
+    this.materials = craft.materials;
+    this.completed = false;
+  }
+  completeCraft() {
+    this.completed = true;
+    return "All done! It looks great!"
+  }
+  calculateProjectTotal() {
+    var totalAmount = 0;
+    for (var i = 0; i < this.materials.length; i++) {
+      totalAmount += this.materials[i].calculateMaterialCost();
+    }
+    return totalAmount;
+  }
 }
 
 module.exports = Craft;
