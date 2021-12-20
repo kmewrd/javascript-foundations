74bf3999b71186adcfabe3c3bb0e58e2de816a79
Kim Ward <kmewrd@outlook.com>
2021-12-20 14:04:00 -0600

    Complete material test

--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,20 @@
 class Material {
-
+  constructor(name, price, amt, units) {
+    this.name = name;
+    this.price = price;
+    this.amount = amt;
+    this.units = units;
+  }
+  useMaterial(amt) {
+    if (this.amount > amt) {
+      this.amount -= amt;
+      return `You now have ${this.amount} ${this.units} of ${this.name} left.`
+    }
+    return `You don't have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+  }
+  calculateMaterialCost() {
+    return this.price * this.amount;
+  }
 }
 
 module.exports = Material;
