895c24de38e6e8b13869f8934bec82b028ad98bf
Kim Ward <kmewrd@outlook.com>
2021-12-20 14:17:33 -0600

    Complete runner test

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,23 @@
 class Runner {
-
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
+  runSomeMiles(miles) {
+    this.milesRun += miles;
+    this.fitness += miles;
+  }
+  stretch() {
+    this.fitness += 0.5;
+  }
+  runRace(name, distance) {
+    this.completedRaces.push(name);
+    this.milesRun += distance;
+    this.fitness += distance;
+  }
 }
 
 module.exports = Runner;
