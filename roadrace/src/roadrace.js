454988486bde081682395ca0a9019bd17c77d779
Kim Ward <kmewrd@outlook.com>
2021-12-22 17:34:42 -0600

    Complete roadrace test

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,22 @@
 class Roadrace {
-
+  constructor(race) {
+    this.name = race.title;
+    this.location = race.city;
+    this.participants = [];
+  }
+  setDistance(miles) {
+    this.distance = miles;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  }
+  completeRace() {
+    for (var i = 0; i < this.participants.length; i++) {
+      this.participants[i].completedRaces.push(this.name);
+      this.participants[i].runSomeMiles(this.distance);
+    }
+  }
 }
 
 module.exports = Roadrace;
