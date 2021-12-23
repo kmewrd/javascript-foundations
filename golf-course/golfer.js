775a4dc267dc43b6b380431b8dbf7a930d8854e6
Kim Ward <kmewrd@outlook.com>
2021-12-22 18:01:42 -0600

    Complete golf course test

--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,40 @@
 class Golfer {
-
-
+  constructor(golfer) {
+    this.name = golfer.name;
+    this.handicap = golfer.handicap;
+    this.frustration = 0;
+    this.confidence = 0;
+  }
+  calculateAvg(par) {
+    return `I usually shoot a ${par + this.handicap} on average.`
+  }
+  playRound(course) {
+    if (course.difficulty === 'hard') {
+      this.frustration += 500;
+    } else {
+      this.frustration += 100;
+    }
+  }
+  hitTheRange() {
+    this.confidence += 10;
+  }
+  marvel(course) {
+    return `I love the ${course.features[0]} and ${course.features[1]} on this course!`
+  }
+  whatYaShoot(score) {
+    if (score > 0) {
+      this.frustration += 20;
+      return 'Doh!'
+    }
+    if (!score) {
+      this.frustration -= 10;
+      return 'Booyah!'
+    }
+    if (score < 0) {
+      this.frustration = 0;
+      return 'I AM THE GREATEST GOLFER ALIVE!'
+    }
+  }
 }
 
 module.exports = Golfer;
