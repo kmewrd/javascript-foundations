c639a73b8ad250e571b7c4fc2575ede82310d0d0
Kim Ward <kmewrd@outlook.com>
2022-01-05 09:32:30 -0600

    Complete gear test

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,17 @@
 class Gear {
-
+  constructor(type, qty) {
+    this.type = type;
+    this.quantity = qty;
+  }
+  checkForValidity() {
+    if (this.type === "food" || this.type === "ammunition" || this.type === "clothes") {
+      return `Great, we'll need lots of ${this.type}!`;
+    } else {
+      var invalidType = this.type;
+      this.type = null;
+      return `I don't think a ${invalidType} will help us.`
+    }
+  }
 }
 
 module.exports = Gear;
